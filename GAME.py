bc42695128d11ea7fe036e7d81cf0c1277e7038a
sg0911 <ymca0814@gmail.com>
2026-03-17 14:24:25 +0900

    pygame 추가

---
+++ GAME.py
@@ -0,0 +1,21 @@
+import pygame
+import sys
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("My First Pygame")
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+clock = pygame.time.Clock()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill(WHITE)
+    pygame.draw.circle(screen, BLUE, (400, 300), 50)
+    
+    pygame.display.flip()
+    clock.tick(60)
+    
+pygame.quit()
+sys.exit()
